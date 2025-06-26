93993832aa7703cdafad3cad0d055917a22ad709
dan <danial.hedaiat@gmail.com>
2025-06-26 19:49:37 +0330

    create BoardGameRanking model

--- boardgame/models.py
+++ boardgame/models.py
@@ -15,3 +15,9 @@ class Tutorial(models.Model):
     title = models.CharField(max_length=100)
     boardgame = models.ForeignKey(BoardGame, on_delete=models.SET_NULL)
     created_by = models.ForeignKey(User, on_delete=models.SET_NULL)
+
+
+class BoardGameRanking(models.Model):
+    rank = models.PositiveSmallIntegerField()
+    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+    boardgame = models.ForeignKey(BoardGame, on_delete=models.SET_NULL)
