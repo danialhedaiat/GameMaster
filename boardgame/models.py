0e22980d8bc166f8dfcd062d7f78eab6e0b8202f
dan <danial.hedaiat@gmail.com>
2025-06-26 19:48:48 +0330

    create component tag model

--- boardgame/models.py
+++ boardgame/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class ComponentTag(models.Model):
+    component = models.ForeignKey(BoardGameComponent, on_delete=models.CASCADE, related_name='tags')
+    tag = models.CharField(max_length=100)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"{self.tag} for {self.component.name} component"
