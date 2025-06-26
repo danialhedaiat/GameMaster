5514fb5092d98033a27a5ed0775ee79ecfa16632
dan <danial.hedaiat@gmail.com>
2025-06-26 19:49:16 +0330

    create Tutorial model

--- boardgame/models.py
+++ boardgame/models.py
@@ -9,3 +9,9 @@ class ComponentTag(models.Model):
 
     def __str__(self):
         return f"{self.tag} for {self.component.name} component"
+
+
+class Tutorial(models.Model):
+    title = models.CharField(max_length=100)
+    boardgame = models.ForeignKey(BoardGame, on_delete=models.SET_NULL)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL)
